fd0733fcc6b324ca7c6083551696beb9ed83784c
Amruta Sharad Patil <amruta.sharad.patil@gmail.com>
2026-05-13 22:00:55 +0530

    Add files via upload

---
+++ RTO_Portal/src/rto_portal/EligiblityCheck.java
@@ -0,0 +1,30 @@
+package rto_portal;
+
+
+
+public class EligiblityCheck {
+	
+	void checkEligiblityForLicence(int age) {
+		
+		if( age < 0 && age > 110 ) {
+			
+			System.out.println("Enter a valid age");
+			
+		}
+		
+		if( age >= 18 && age < 75 ) {
+			
+			System.out.println("You are eligible for the licence");
+			
+		}else if( age >= 75 ) {
+			
+			System.out.println("You are not eligible for the licence");
+			
+		}else  {
+			
+			int waitingPeriod=18-age;
+			System.out.println("wait for "+ waitingPeriod+" years for Licence");
+			
+		}
+	}
+}
---
+++ RTO_Portal/src/rto_portal/RTO_Portal.java
@@ -0,0 +1,19 @@
+package rto_portal;
+import java.util.Scanner;
+public class RTO_Portal {
+
+	public static void main(String[] args) {
+		
+		EligiblityCheck e=new EligiblityCheck();
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter your age to check you eligiblity for Licence:");
+        int age=sc.nextInt();
+		
+		e.checkEligiblityForLicence(age);
+		
+		sc.close();
+	}
+
+}
